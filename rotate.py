65fafb72d4a58176ecca55861e48a6cfbb39e8e0
zelatyna <magda.sendal@gmail.com>
2020-02-04 16:07:02 +0100

    Chapter 6.1

---
+++ rotate.py
@@ -0,0 +1,25 @@
+import numpy as np, argparse, imutils, cv2
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True,
+help = "Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
+
+(h, w) = image.shape[:2]
+center = (w // 2, h // 2)
+
+M = cv2.getRotationMatrix2D(center, 45, 1.0)
+rotated = cv2.warpAffine(image, M, (w, h))
+cv2.imshow("Rotated by 45 Degrees", rotated)
+
+M = cv2.getRotationMatrix2D(center, -90, 1.0)
+rotated = cv2.warpAffine(image, M, (w, h))
+cv2.imshow("Rotated by -90 Degrees", rotated)
+
+rotated = imutils.rotate(image, 180)
+cv2.imshow("Rotated by 180 Degrees", rotated)
+cv2.waitKey(0)
